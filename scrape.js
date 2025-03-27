70e874027f2f73afc3c46044f0b0bdcf68246905
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2025-03-27 09:59:45 +0100

    Added a conditional loop to fetch data from 10 pages using the website pagination.

--- scrape.js
+++ scrape.js
@@ -5,31 +5,45 @@ const scraper = async() =>{
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    const url = 'https://books.toscrape.com';
+    // Initiate empty array for all pages books
+    const allBooks = [];
+    // Get pagination values
+    let currentPage = 1;
+    const maxPages = 10;
 
-    await page.goto(url);
+    while(currentPage <= maxPages){
+        const url = `https://books.toscrape.com/catalogue/page-${currentPage}.html`;
 
-    const books = await page.evaluate(()=>{
-        const bookElements = document.querySelectorAll('.product_pod');
-        return Array.from(bookElements).map((book)=>{
-            const title = book.querySelector('h3 a').getAttribute('title');
-            const price = book.querySelector('.price_color').textContent;
-            const stock = book.querySelector('.instock.availability') ? 'In Stock' : 'Out of Stock';
-            const rating = book.querySelector('.star-rating').className.split(' ')[1];
-            const link = book.querySelector('h3 a').getAttribute('href');
-
-            return {
-                title,
-                price,
-                stock,
-                rating,
-                link
-            };
+        await page.goto(url);
+    
+        const books = await page.evaluate(()=>{
+            const bookElements = document.querySelectorAll('.product_pod');
+            return Array.from(bookElements).map((book)=>{
+                const title = book.querySelector('h3 a').getAttribute('title');
+                const price = book.querySelector('.price_color').textContent;
+                const stock = book.querySelector('.instock.availability') ? 'In Stock' : 'Out of Stock';
+                const rating = book.querySelector('.star-rating').className.split(' ')[1];
+                const link = book.querySelector('h3 a').getAttribute('href');
+    
+                return {
+                    title,
+                    price,
+                    stock,
+                    rating,
+                    link
+                };
+            });
         });
-    });
+
+        allBooks.push(...books);
+        console.log(`Books on Page ${currentPage}:`, books);
+        currentPage++;
+    }
+
+
 
     // Save as Json file
-    fs.writeFileSync('scraped-books.json', JSON.stringify(books, null, 2));
+    fs.writeFileSync('scraped-books.json', JSON.stringify(allBooks, null, 2));
 
     console.log("Books Data Created");
     
